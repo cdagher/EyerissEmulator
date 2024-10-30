8183f6dcc7552f32b8a4b3b027403253919f16b8
Chris Dagher <chrisdagher642@gmail.com>
2024-10-30 10:39:44 -0600

    First attempt at a Data class

--- eyeriss/src/data/data.py
+++ eyeriss/src/data/data.py
@@ -1,2 +1,39 @@
+from src import Instruction
+
+import numpy as np
+
 class Data:
-    pass
+    _instr: Instruction
+    _data: np.ndarray
+
+    def __init__(self, instr: int, data: int):
+        self._instr = instr
+        self._data = data
+
+    @property
+    def instr(self) -> Instruction:
+        return self._instr
+    
+    @property
+    def data(self) -> np.ndarray:
+        return self._data
+    
+    def __repr__(self):
+        return f"Data(instr={self._instr}, data={self._data})"
+    
+    def __eq__(self, other):
+        if isinstance(other, Data):
+            return self._instr == other._instr and self._data == other._data
+        return False
+    
+    def __hash__(self):
+        return hash((self._instr, self._data))
+    
+    def __lt__(self, other):
+        if isinstance(other, Data):
+            return self._instr < other._instr
+        return NotImplemented
+    
+    def __str__(self) -> str:
+        return f"Data(instr={self._instr}, data={self._data})"
+    
