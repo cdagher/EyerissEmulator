9be1d1ae1427ee7ed5e05a56c26684dc2cc4d9d9
Chris Dagher <chrisdagher642@gmail.com>
2024-10-30 10:36:03 -0600

    Initial instruction set

--- eyeriss/src/__init__.py
+++ eyeriss/src/__init__.py
@@ -5,4 +5,18 @@ from noc import NoC
 from memory import Memory, Hierarchy
 from pe import PE
 
+from enum import Enum
 
+
+class Instruction(Enum):
+    READ = 1
+    WRITE = 2
+    COMPUTE = 3
+    PE_WRITE_FILTER = 4
+    PE_WRITE_IFMAP = 5
+    PE_Write_IFMAP = 6
+    PE_READ_PSUM = 7
+
+
+def relu(x):
+    return max(0, x)
